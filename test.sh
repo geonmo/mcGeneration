28114a2744c177e535dc37d550c9e40bce3aa5ab
Geonmo <ry840901@gmail.com>
2016-01-19 00:53:10 +0900

    Fix bug.

--- test.sh
+++ test.sh
@@ -3,6 +3,5 @@ source /opt/mcGeneration/env.sh
 tar -zxvf gridpack.tar.gz
 ./run.sh $1 $2 $3
 hostname
-LAST_DIR=`pwd | awk -F'/' '{print $NF}'`
-echo $LAST_DIR
+LAST_DIR="TT0J"
 xrdcp delphes.root root://cms-xrdr.sdfarm.kr//xrd/store/user/geonmo/gridpack/$LAST_DIR/delphes_$2.root 
