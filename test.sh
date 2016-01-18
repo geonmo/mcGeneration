ca3795d2986884f4c6d967e02a751dcc02cdcc98
Geonmo <ry840901@gmail.com>
2016-01-18 19:00:05 +0900

    Add xrdcp option.

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 source /opt/mcGeneration/env.sh
 tar -zxvf gridpack.tar.gz
-./run.sh $1 $2 $3 
+./run.sh $1 $2 $3
+hostname
+LAST_DIR=`pwd | awk -F'/' '{print $NF}'`
+echo $LAST_DIR
+xrdcp delphes.root root://cms-xrdr.sdfarm.kr//xrd/store/user/geonmo/gridpack/$LAST_DIR/delphes_$2.root 
